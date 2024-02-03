1df7d4e1c25c182826f769805eafb81afb334d8a
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2024-02-03 07:10:48 -0500

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
+import Movies from './Movies/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 };
