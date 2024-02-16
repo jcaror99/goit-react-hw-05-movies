80def3b688031b9e9eeddeaa8a0361da22efd8d9
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2024-02-15 22:18:39 -0500

    lazyLoad

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, NavLink } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Home from './Home/Home';
 import Movies from './Movies/Movies';
 import NotFound from './NotFound/NotFound';
 import css from './App.module.css';
 import MoviesDetail from './MoviesDetail/Moviesdetail';
-import Cast from './Cast/Cast';
 import Reviews from './Reviews/Reviews';
+
+const Cast = lazy(() => import('./Cast/Cast'));
 
 export const App = () => {
   return (
@@ -18,17 +20,18 @@ export const App = () => {
           Movies
         </NavLink>
       </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movies" element={<Movies />}></Route>
-        <Route path="/movies?query=:movies" element={<Movies />}></Route>
-        <Route path="/movies/:moviesId" element={<MoviesDetail />}>
-          <Route path="cast" element={<Cast />}></Route>
-          <Route path="reviews" element={<Reviews />}></Route>
-        </Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Suspense fallback={<div>Loading please wait...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/movies?query=:movies" element={<Movies />}></Route>
+          <Route path="/movies/:moviesId" element={<MoviesDetail />}>
+            <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route>
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
